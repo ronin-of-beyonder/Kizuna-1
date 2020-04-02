8a1e267d8ce122bc5305e86eab718d638b0a4536
Neilson Keith Gardose <neilsongardose@gmail.com>
2020-04-02 16:52:58 +0800

    setup structure of ui along with the connection to the dna

---
+++ ui/src/graphql/schema.js
@@ -0,0 +1,8 @@
+import gql from "graphql-tag";
+
+export default gql`
+  type FirstName {
+    id: ID
+    name: String
+  }
+`;
--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,54 +1,30 @@
-import {IonContent,
+import {
+  IonContent,
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar,
-  IonList,
-  IonItem,
-  IonLabel,
-  IonCheckbox,
-  IonNote,
-  IonBadge,
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonInput,
-  IonButton
-} from '@ionic/react';
-import React, {useState} from 'react';
-import {  pizza } from 'ionicons/icons';
-import './Home.css';
+  IonToolbar
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
-const Home: React.FC = () => {
-  const [text, setText] = useState('');
-
-  return (
-    <IonPage>
-      <IonHeader>
+const Home: React.FC = () => (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Blank</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Kizuna Connect</IonTitle>
+          <IonTitle size="large">Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Kizuna Connect</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
-          <IonItem>
-            <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)}></IonInput>
-          </IonItem>
-        </IonList>
-
-        <IonButton size={'large'}>
-          <IonIcon slot="start" icon={pizza} />
-          Submit
-        </IonButton>
-      </IonContent>
-    </IonPage>
-  );
-};
+      <ExploreContainer />
+    </IonContent>
+  </IonPage>
+);
 
 export default Home;
